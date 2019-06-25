dfb344486867d035fa1fe24c9b2ea28640404acc
Carlos <carlosjosechiquillolopez@gmail.com>
2019-06-25 10:40:38 -0600

    adding date fild to form view

--- src/main/java/com/utec/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/utec/springboot/app/models/entity/Cliente.java
@@ -8,7 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
+//import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -31,10 +31,10 @@ public class Cliente implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@PrePersist
+	/*@PrePersist
 	public void prePersist() {
 		createAt = new Date();
-	}
+	}*/
 	
 	public Long getId() {
 		return id;
