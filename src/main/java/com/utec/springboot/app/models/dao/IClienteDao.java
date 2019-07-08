7a9808fc4c8d19878ab01bc0175b17297a656f7f
Carlos <carlosjosechiquillolopez@gmail.com>
2019-07-08 10:07:11 -0600

    adding paginator

--- src/main/java/com/utec/springboot/app/models/dao/IClienteDao.java
+++ src/main/java/com/utec/springboot/app/models/dao/IClienteDao.java
@@ -1,11 +1,11 @@
 package com.utec.springboot.app.models.dao;
 
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.utec.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long>{
+public interface IClienteDao extends PagingAndSortingRepository<Cliente, Long>{
 	
 	
 }
